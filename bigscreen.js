c2682582096445a1488144b403bb4a657c9f2bfe
mark <mark@ip-10-112-59-206.ec2.internal>
2011-02-20 03:03:53 +0000

    fix to screen id

--- bigscreen.js
+++ bigscreen.js
@@ -151,7 +151,7 @@ Show.prototype = {
 	    var screen = new Screen("screen" + this.screenSequence,[this.photos[0]]);
 	    this.photos.shift();
 	    this.screenSequence++;
-	    screen.content = '<img class="screenImg visible" id="' + this.id + '" src="' + this.photos[0].url + '" />';
+	    screen.content = '<img class="screenImg visible" id="' + screen.id + '" src="' + screen.photos[0].url + '" />';
 	    this.screens.push(screen);
 	    return screen;
         } else {
