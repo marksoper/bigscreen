b04ec6d925a8aebe1bee28952b5ed5e73f14bd73
mark <mark@ip-10-112-59-206.ec2.internal>
2011-03-03 19:19:56 +0000

    append

--- bigscreen.js
+++ bigscreen.js
@@ -244,7 +244,7 @@ Show.prototype = {
     },
     makeScreen : function(photo) {
 	var screen = new Screen(this.screenSequence, [photo]);
-	screen.content = '<div class="screenDiv" id="screenDiv' + screen.id + '" ><img class="screenImg" id="screenImg' + screen.id + '" src="' + screen.photos[0].url + '" /></div>';
+	screen.content = '<div class="screenDiv hidden" id="screenDiv' + screen.id + '" ><img class="screenImg" id="screenImg' + screen.id + '" src="' + screen.photos[0].url + '" /></div>';
 	return screen;
     },
     insertScreen : function(screen) {  
